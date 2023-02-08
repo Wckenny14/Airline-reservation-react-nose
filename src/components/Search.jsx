7950e8d516b0f2359f5fd2bad28896b13b37dedd
Wayne Kenny <waynekenny@Waynes-MacBook-Air.local>
2023-02-08 14:38:22 +1100

    third commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,7 @@
 import React, { Component, useState } from 'react';
 import axios from 'axios';
 
-const SERVER_URL = 'http://localhost:3000/flights.json'; // Later: change this heroku or wherever you host your server
+const SERVER_URL = 'http://localhost:3000/'; // Later: change this heroku or wherever you host your server
 
 class Search extends Component {
     constructor() {
@@ -10,20 +10,21 @@ class Search extends Component {
             flights: [],
             airplanes: []
         };
-        this.saveFlight = this.saveFlight.bind(this);
-        this.saveAirplane = this.saveAirplane.bind(this);
+        // this.saveFlight = this.saveFlight.bind(this);
+        // this.saveAirplane = this.saveAirplane.bind(this);
     }
     // React life cycle method:
     componentDidMount() {
         const fetchFlights = () => {
-            axios.get(SERVER_URL + 'flights.json').then((response) => {
+            axios(SERVER_URL + 'flights.json').then((response) => {
                 this.setState({flights: response.data}); // set the data from the API as our state
-                console.log(response) // tells us where to retrieve data
+                // console.log(response) // tells us where to retrieve data
                 setTimeout(fetchFlights, 5000); // recursion for polling. Setting timer to call itself again in 7 seconds
+                // console.log(fetchFlights)
             });
         };
         const fetchAirplanes = () => {
-            axios.get(SERVER_URL + 'airplanes.json').then((response) => {
+            axios(SERVER_URL + 'airplanes.json').then((response) => {
                 this.setState({airplanes: response.data}); // set the data from the API as our state
                 console.log(response) // tells us where to retrieve data
                 setTimeout(fetchAirplanes, 5000); // recursion for polling. Setting timer to call itself again in 7 seconds
@@ -33,17 +34,17 @@ class Search extends Component {
         fetchAirplanes();
     }
 
-    saveFlight(flightInfo) {
-        axios.post(SERVER_URL, { flightInfo: flightInfo }).then((response) => {
-            this.setState({flights: [response.data, ...this.state.flights]});
-        });
-    }
+    // saveFlight(flightInfo) {
+    //     axios.post(SERVER_URL, { flightInfo: flightInfo }).then((response) => {
+    //         this.setState({flights: [response.data, ...this.state.flights]});
+    //     });
+    // }
 
-    saveAirplane(airplaneInfo) {
-        axios.post(SERVER_URL, { airplaneInfo: airplaneInfo }).then((response) => {
-            this.setState({airplanes: [response.data, ...this.state.flights]});
-        });
-    }
+    // saveAirplane(airplaneInfo) {
+    //     axios.post(SERVER_URL, { airplaneInfo: airplaneInfo }).then((response) => {
+    //         this.setState({airplanes: [response.data, ...this.state.flights]});
+    //     });
+    // }
 
     render() {
         return (
@@ -57,15 +58,41 @@ class Search extends Component {
 
 const FlightForm = (props) => {
     return (
-        <div>
-            <h2>{ props.flights.length } Burning Airlines</h2>
+        <div id="flightTable">
+            <h2>{ props.flights.length } Flights Available</h2>
+            <table style={{width: "100%"}}>
+                <thead>
+                    <tr>
+                        <th>Flight No.</th>
+                        <th>Origin</th>
+                        <th>Destination</th>
+                        <th>Date</th>
+                        {/* <th>Airplane</th> */}
+                        {/* <th>Available Seats</th> */}
+                        <th colSpan="6"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { props.flights.map((f) => {
+                        return (
+                            <tr key={ f.id }>
+                                <td>{ f.flight_number }</td>
+                                <td>{ f.origin }</td>
+                                <td>{ f.destination }</td>
+                                <td>{ f.date }</td>
+                                {/* <td>{ f.airplane_id.plane_name }</td> */}
+                            </tr>
+                        );
+                    })};
+                </tbody>
+            </table>
         </div>
     )
 }
 const AirplaneForm = (props) => {
     return (
         <div>
-            <h2>{ props.airplanes.length } Burning Airlines</h2>
+            {/* <h2>{ props.airplanes.length } Airplanes</h2> */}
         </div>
     )
 }
